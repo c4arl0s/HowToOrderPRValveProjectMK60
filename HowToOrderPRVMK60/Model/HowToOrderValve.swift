9960738f2bdd21b7d3634309c69b0d6009d9bfa8
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2018-12-22 12:54:20 -0600

    add more properties to HowToOrderValve struct

--- HowToOrderPRVMK60/Model/HowToOrderValve.swift
+++ HowToOrderPRVMK60/Model/HowToOrderValve.swift
@@ -12,11 +12,19 @@ struct HowToOrderValve {
     var model: Model
     var size: Size
     var bodyMaterial: BodyMaterial
+    var endConnection: Endconnection
+    var trim: Trim
+    var seatMaterial: SeatMaterial
+    var cv: Cv
     
-    init(model: Model, size: Size, bodyMaterial: BodyMaterial) {
+    init(model: Model, size: Size, bodyMaterial: BodyMaterial, endConnection: Endconnection, trim: Trim, seatMaterial: SeatMaterial, cv: Cv ) {
         self.model = model
         self.size = size
         self.bodyMaterial = bodyMaterial
+        self.endConnection = endConnection
+        self.trim = trim
+        self.seatMaterial = seatMaterial
+        self.cv = cv
     }
     static func getAllCharacteristics() -> [String] {
         let arrayOfCharacteriscs = [ "Model",
